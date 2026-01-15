ba9e80e64021463dd2052c489f9ee132164dcee8
Ravi Project <ravicorpnetwork@gmail.com>
2026-01-15 15:55:55 +0530

    fix mongodb env variable name

--- db.js
+++ db.js
@@ -5,7 +5,7 @@
 //   .catch((err) => console.log("MongoDB Error:", err));
 const mongoose = require("mongoose");
 
-mongoose.connect(process.env.DB_URL)
+mongoose.connect(process.env.MONGODB_URL)
   .then(() => console.log("MongoDB Connected"))
   .catch(err => console.log("MongoDB Error:", err));
 
